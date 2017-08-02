ea90415476b0347b5ea723391acf82d74f5e3d94
Vicuko <victorc.fernandez@gmail.com>
2017-08-02 22:40:37 +0200

    Create layout for item elements

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -3,6 +3,7 @@ package com.example.android.quakereport;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -22,6 +23,14 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listView = convertView;
+        if (listView == null){
+            listView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent,false);
+        }
+
+        Earthquake earthquake = getItem(position);
+
+
         return super.getView(position, convertView, parent);
     }
 }
